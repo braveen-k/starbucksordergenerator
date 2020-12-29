9a1c1ad3ebf289a7aa95b1f6bf70c35da0227496
Glitch (hello-webpage) <none>
2020-12-29 16:09:10 +0000

    ⏰🙌 Checkpoint ./index.html:22903038/5886 ./script.js:22903038/2130 ./style.css:22903038/2265

--- script.js
+++ script.js
@@ -1,5 +1,60 @@
-/* If you're feeling fancy you can add interactivity 
-    to your site with Javascript */
+//Calling DOM
 
-// prints "hi" in the browser's dev tools console
-console.log("hi");
+let generateButton = document.getElementById('generate');
+
+let order = document.getElementById('order');
+
+let forgot = document.getElementById('forgot')
+
+const buttonPress = () => {
+  generate.style.backgroundColor = 'white';
+  generate.style.color = 'black';
+}
+
+const buttonRelease = () => {
+  generate.style.backgroundColor = '';
+  generate.style.color = '';
+}
+
+
+const starbucksSize = ["tall", "venti", "grande"]
+const starbucksDrink = ["tea", "latte", "coffee", "cold brew", "espresso", "refresher", "hot chocolate", "flat white", "crème"]
+const starbucksType = ["frappe", "mocha", "frappuccino", "kiwi starfruit", "mango dragonfruit"]
+const starbucksAdjective = ["vanilla bean", "cinnamon", "salted caramel", "pumpkin spice", "Irish cream"]
+
+const starbucksExtra = ["with extra whip", "with extra ice", "with less ice", "no foam", "half sweet"]
+
+const starbucksForgot = ["ginger molasses cookie", "oat fudge bar", "cheese and fruit protein box", "butter croissant", "cake pop", "peanut butter cookie brownie", "snowman cookie", "gingerbread biscotti"]
+
+
+const starbucksOrder = () => {
+  let size = starbucksSize[Math.floor(Math.random() * starbucksSize.length)];
+  let drink = starbucksDrink[Math.floor(Math.random() * starbucksDrink.length)];
+  let type = starbucksType[Math.floor(Math.random() * starbucksType.length)];
+  let adjective = starbucksAdjective[Math.floor(Math.random() * starbucksAdjective.length)];
+  let extra = starbucksExtra[Math.floor(Math.random() * starbucksExtra.length)];  
+  let ohforgot = starbucksForgot[Math.floor(Math.random() * starbucksForgot.length)];
+  
+order.innerHTML= ("Can I order a " + size + ' ' + adjective +' ' + type + ' ' + drink + ' '+ extra + '?'); 
+  
+forgot.innerHTML = (`Oh, and a ${ohforgot} too please🖤`);
+
+}
+
+generateButton.addEventListener("mousedown", buttonPress);
+
+
+generateButton.addEventListener("mouseup", buttonRelease);
+
+
+generateButton.addEventListener("click", starbucksOrder);
+
+
+
+
+
+
+
+
+
+
